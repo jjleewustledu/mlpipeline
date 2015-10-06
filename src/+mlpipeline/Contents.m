6cc50ed21fbcbca34d06dcf6743c0d1a4cb058d4
John Lee <jjlee.wustl.edu@gmail.com>
2015-10-06 15:37:12 -0500

    Recovered functionality of TerritoryGlmDirector for Colin's Moyamoya paper.

---
+++ src/+mlpipeline/Contents.m
@@ -0,0 +1,22 @@
+% +MLPIPELINE
+%
+% Files
+%   AbstractOptions          - 
+%   AnalysisArrayBuilder     - is a builder design pattern that builds Matlab arrays for data mining%  Version $Revision: 1604 $ was created $Date: 2012-08-23 23:09:13 -0500 (Thu, 23 Aug 2012) $ by $Author: jjlee $,  
+%   AnalysisBuilder          - is a builder design pattern for imaging analysis objects%  Version $Revision: 1605 $ was created $Date: 2012-08-23 23:09:13 -0500 (Thu, 23 Aug 2012) $ by $Author: jjlee $,  
+%   AnalysisDirector         - works with a builder design pattern for data analysis%  Version $Revision: 1606 $ was created $Date: 2012-08-23 23:09:13 -0500 (Thu, 23 Aug 2012) $ by $Author: jjlee $,  
+%   ConsoleLogger            - 
+%   DataExplorer             - provides methods to survey imaging data from study populations
+%   ImproperParfor           - executes parfor in clusters sized to the avilable matlabpool
+%   Logger                   - accumulates logging strings in a CellArrayList
+%   LoggerInterface          - 
+%   Np755                    - 
+%   Np755Builder             - is a builder design pattern for analysis of study np755
+%   Patient                  - is a container class for all data associated with a study-patient
+%   PipelineRegistry         - is a singleton design pattern	
+%   PipelineVisitor          - 
+%   PipelineVisitorInterface - 
+%   Reporter                 - accumulates string-data
+%   SilentTests              - is intended for use in continuous integration
+%   StudyAlignmentDirector   - orchestration alignemnt operations for entire NP-studies
+%   SummaryTable             - is a builder pattern for reporting results
