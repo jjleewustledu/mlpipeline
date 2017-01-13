d8a93a02ada7edcfa2472d19f96b05433806963e
John Lee <jjlee.wustl.edu@gmail.com>
2017-01-12 19:25:30 -0600

    fourdfp_unittest.Test_T4ResolveBuilder appears to be working:  this check-in is a WIP.

---
+++ src/+mlpipeline/IImageBuilder.m
@@ -0,0 +1,28 @@
+classdef IImageBuilder < mlpipeline.IDataBuilder
+	%% IIMAGEBUILDER  
+
+	%  $Revision$
+ 	%  was created 06-Jan-2017 18:33:36
+ 	%  by jjlee,
+ 	%  last modified $LastChangedDate$
+ 	%  and checked into repository /Users/jjlee/Local/src/mlcvl/mlpipeline/src/+mlpipeline.
+ 	%% It was developed on Matlab 9.1.0.441655 (R2016b) for MACI64.
+ 	
+
+	properties (Abstract)
+        firstCrop
+        frames 
+        inverseCrop        
+        referenceImage
+        referenceWeight
+        sourceImage
+        sourceWeight
+ 	end
+
+	methods 
+		  
+ 	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
