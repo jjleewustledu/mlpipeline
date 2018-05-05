9336f996f5ec3b6821047c1eb8230e49a7c2bd78
John J. Lee <jjlee.wustl.edu@gmail.com>
2018-05-04 22:51:52 -0500

    Initial check-in.

---
+++ src/+mlpipeline/IStudyCensus.m
@@ -0,0 +1,20 @@
+classdef IStudyCensus 
+	%% ISTUDYCENSUS  
+
+	%  $Revision$
+ 	%  was created 04-May-2018 16:20:26 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlpipeline/src/+mlpipeline.
+ 	%% It was developed on Matlab 9.4.0.813654 (R2018a) for MACI64.  Copyright 2018 John Joowon Lee.
+ 	
+	properties (Abstract)
+ 		censusTable
+        sessionData
+ 	end
+
+	methods 
+		  
+  	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
