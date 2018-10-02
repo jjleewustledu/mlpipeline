88d276b3c335ffa07d1a727fb47851f9be5baedb
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2018-10-01 23:57:00 -0500

    Removing most uses of loggerFilename | loggerFileprefix.

---
+++ src/+mlpipeline/ILogger.m
@@ -0,0 +1,28 @@
+classdef (Abstract) ILogger < handle & matlab.mixin.Copyable & mlpatterns.List
+	%% ILOGGER  
+
+	%  $Revision$
+ 	%  was created 28-Sep-2018 16:47:50 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlpipeline/src/+mlpipeline.
+ 	%% It was developed on Matlab 9.5.0.944444 (R2018b) for MACI64.  Copyright 2018 John Joowon Lee.
+ 	
+    properties (Abstract, Constant)
+        FILETYPE_EXT
+        DATESTR_FORMAT
+        TIMESTR_FORMAT
+    end
+    
+	properties (Abstract)
+        callerid
+        contents
+        creationDate
+        echoToCommandWindow
+        hostname
+        includeTimeStamp 
+        id % user id
+        uname		
+ 	end
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
