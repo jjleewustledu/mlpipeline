2aacda0cfa2351f8fca4a861e4dd9a67ee518ef9
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-05-24 15:45:30 -0500

    moved up properties and legacy methods

--- src/+mlpipeline/StudyData.m
+++ src/+mlpipeline/StudyData.m
@@ -7,13 +7,47 @@ classdef (Abstract) StudyData < handle & mlpipeline.IStudyData
  	%  last modified $LastChangedDate$
  	%  and checked into repository /Users/jjlee/Local/src/mlcvl/mlpipeline/src/+mlpipeline.
  	%% It was developed on Matlab 9.0.0.307022 (R2016a) Prerelease for MACI64.
- 	
+ 	    
+    properties (Dependent)
+        atlVoxelSize
+        dicomExtension
+        noclobber
+        
+        rawdataDir
+        projectsDir
+        subjectsDir
+        YeoDir
+    end
     
     properties
         comments
     end
     
-	methods        
+	methods 
+        
+        %% GET
+        
+        function g = get.atlVoxelSize(this)
+            g = this.registryInstance.atlVoxelSize;            
+        end
+        function g = get.dicomExtension(this)
+            g = this.registryInstance.dicomExtension;
+        end
+        function g = get.noclobber(this)
+            g = this.registryInstance.noclobber;
+        end
+        function g = get.rawdataDir(this)
+            g = this.registryInstance.rawdataDir;
+        end
+        function g = get.projectsDir(this)
+            g = this.registryInstance.projectsDir;
+        end
+        function g = get.subjectsDir(this)
+            g = this.registryInstance.subjectsDir;
+        end
+        function g = get.YeoDir(this)
+            g = this.registryInstance.YeoDir;
+        end       
         
         %%
         
@@ -37,6 +71,65 @@ classdef (Abstract) StudyData < handle & mlpipeline.IStudyData
             parse(ip, varargin{:});
             loc = fullfile(ip.Results.path, sprintf('%s_workspace_%s.mat', mfilename, mydatetimestr(now)));
             save(loc);
+        end
+        
+        %%
+        
+ 		function this = StudyData(varargin)
+            ip = inputParser;
+            addRequired( ip, 'registry')
+            addParameter(ip, 'dicomExtension', '', @ischar);
+            addParameter(ip, 'rawdataDir', '', @ischar);
+            addParameter(ip, 'projectsDir', '', @ischar);
+            addParameter(ip, 'subjectsDir', '', @ischar);
+            addParameter(ip, 'YeoDir', '', @ischar);
+            parse(ip, varargin{:});
+            
+            this.registryInstance = mlraichle.RaichleRegistry.instance();
+            if ~isempty(ip.Results.dicomExtension)
+                this.registryInstance.dicomExtension = ip.Results.dicomExtension;
+            end
+            if ~isempty(ip.Results.rawdataDir)
+                this.registryInstance.rawdataDir = ip.Results.rawdataDir;
+            end
+            if ~isempty(ip.Results.projectsDir)
+                this.registryInstance.projectsDir = ip.Results.projectsDir;
+            end
+            if ~isempty(ip.Results.subjectsDir)
+                this.registryInstance.subjectsDir = ip.Results.subjectsDir;
+            end
+            if ~isempty(ip.Results.YeoDir)
+                this.registryInstance.YeoDir = ip.Results.YeoDir;
+            end
+        end 
+    end
+    
+    %% PROTECTED
+    
+    properties (Access = protected)
+        registryInstance
+    end
+    
+    %% HIDDEN LEGACY
+    
+    methods
+        function a    = seriesDicomAsterisk(this, fqdn)
+            assert(isdir(fqdn));
+            assert(isdir(fullfile(fqdn, 'DICOM')));
+            a = fullfile(fqdn, 'DICOM', ['*' this.dicomExtension]);
+        end        
+        function f    = subjectsDirFqdns(this)
+            if (isempty(this.subjectsDir))
+                f = {};
+                return
+            end            
+            dt = mlsystem.DirTools(this.subjectsDir);
+            f = {};
+            for di = 1:length(dt.dns)
+                if (strncmp(dt.dns{di}, 'sub-', 4))
+                    f = [f dt.fqdns(di)]; %#ok<AGROW>
+                end
+            end
         end
     end
     
