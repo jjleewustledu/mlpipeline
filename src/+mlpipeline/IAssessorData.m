b6d2802787cb39fcc039894707ac0336fdaf42b3
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-06-12 18:24:02 -0500

    creating symmetries with mlxnat

---
+++ src/+mlpipeline/IAssessorData.m
@@ -0,0 +1,19 @@
+classdef IAssessorData 
+	%% IASSESSORDATA  
+
+	%  $Revision$
+ 	%  was created 11-Jun-2019 02:59:57 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlpipeline/src/+mlpipeline.
+ 	%% It was developed on Matlab 9.5.0.1067069 (R2018b) Update 4 for MACI64.  Copyright 2019 John Joowon Lee.
+ 	
+	properties
+ 		
+ 	end
+
+	methods 
+		  
+  	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
---
+++ src/+mlpipeline/IResourceData.m
@@ -0,0 +1,19 @@
+classdef IResourceData 
+	%% IRESOURCEDATA  
+
+	%  $Revision$
+ 	%  was created 11-Jun-2019 02:59:48 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlpipeline/src/+mlpipeline.
+ 	%% It was developed on Matlab 9.5.0.1067069 (R2018b) Update 4 for MACI64.  Copyright 2019 John Joowon Lee.
+ 	
+	properties
+ 		
+ 	end
+
+	methods 
+		  
+  	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
