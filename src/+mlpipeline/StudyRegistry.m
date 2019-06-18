f7e50d6cb2f1048f3d5ca6667a5ff051152e8f54
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-06-18 05:18:53 -0500

    refactoring global, persistent data and methods in registries

--- src/+mlpipeline/StudyRegistry.m
+++ src/+mlpipeline/StudyRegistry.m
@@ -7,7 +7,7 @@ classdef (Abstract) StudyRegistry < handle & mlpatterns.Singleton2
  	%% It was developed on Matlab 9.5.0.1067069 (R2018b) Update 4 for MACI64.  Copyright 2019 John Joowon Lee.
  	
     properties     
-        atlVoxelSize = 222   
+        atlVoxelSize = 333   
         comments = ''
         noclobber = true
     end
@@ -15,11 +15,6 @@ classdef (Abstract) StudyRegistry < handle & mlpatterns.Singleton2
 	properties (Dependent)
         debug
         keepForensics
-        
-        rawdataDir
-        projectsDir
-        subjectsDir
-        YeoDir
     end
     
     methods
@@ -37,48 +32,11 @@ classdef (Abstract) StudyRegistry < handle & mlpatterns.Singleton2
             g = mlpipeline.ResourcesRegistry.instance().keepForensics;
         end
         
-        function x = get.rawdataDir(~)
-            x = fullfile(getenv('PPG'), 'rawdata', '');
-        end
-        function g = get.projectsDir(~)
-            g = getenv('PROJECTS_DIR');
-        end        
-        function     set.projectsDir(~, s)
-            assert(isdir(s));
-            setenv('PROJECTS_DIR', s);
-        end
-        function g = get.subjectsDir(~)
-            g = getenv('SUBJECTS_DIR');
-        end        
-        function     set.subjectsDir(~, s)
-            assert(isdir(s));
-            setenv('SUBJECTS_DIR', s);
-        end
-        function g = get.YeoDir(this)
-            g = this.subjectsDir;
-        end
-        
         %%        
         
-        function       diaryOff(~)
-            diary off;
-        end
-        function       diaryOn(this, varargin)
-            ip = inputParser;
-            addOptional(ip, 'path', this.projectsDir, @isdir);
-            parse(ip, varargin{:});            
-            diary(fullfile(ip.Results.path, sprintf('%s_diary_%s.log', mfilename('class'), mydatetimestr(now))));
-        end
         function tf  = isChpcHostname(~)
             [~,hn] = mlbash('hostname');
             tf = lstrfind(hn, 'gpu') || lstrfind(hn, 'node') || lstrfind(hn, 'login') || lstrfind(hn, 'cluster');
-        end
-        function loc = saveWorkspace(this, varargin)
-            ip = inputParser;
-            addOptional(ip, 'path', this.projectsDir, @isdir);
-            parse(ip, varargin{:});
-            loc = fullfile(ip.Results.path, sprintf('%s_workspace_%s.mat', mfilename('class'), mydatetimestr(now)));
-            save(loc);
         end
     end
 
